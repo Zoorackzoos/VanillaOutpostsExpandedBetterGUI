974c4b0d55d0bc9b12de3a5281239d78732ab160
Zoorackzoos <duncancholmes@gmail.com>
2026-06-02 22:29:55 -0500

    dang it to heck

--- Source/VOEBetterPawnUI/obj/Debug/VOEBetterPawnUI.AssemblyInfo.cs
+++ Source/VOEBetterPawnUI/obj/Debug/VOEBetterPawnUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VOEBetterPawnUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21884e28c7774755a17a801325f4a467c93916a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea427e85e1c31bec2e2b74ad745de4adc5875d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VOEBetterPawnUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VOEBetterPawnUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
